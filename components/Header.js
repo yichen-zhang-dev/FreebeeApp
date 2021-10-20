ba20c47ed0a15578beb6345d3129b2f38e33e565
Yichen Zhang <yichenjake@utexas.edu>
2021-10-20 12:09:15 -0400

    tier ranking: a trophy button and a flatlist of top users

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faMap } from "@fortawesome/free-solid-svg-icons";
+import { faMap, faTrophy } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
+export default function Header({ ranking = false, navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "flex-end" }}>
       <Text style={styles.appTitle}>
         FREEBEE
         <FontAwesomeIcon style={{ marginLeft: 24 }} icon={faMap} size={42} />
+        {ranking && (
+          <Pressable onPress={() => navigation.navigate("Ranking")}>
+            <FontAwesomeIcon icon={faTrophy} size={30} color={"#7BBA83"} />
+          </Pressable>
+        )}
       </Text>
     </View>
   );
@@ -17,7 +22,6 @@ export default function Header() {
 const styles = StyleSheet.create({
   appTitle: {
     color: "black",
-    // fontFamily: "monospace",
     fontSize: 45,
   },
 });
