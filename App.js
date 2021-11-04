63e6dc00ccbf61bea792b0ca14b4c243fc237b4d
Yichen Zhang <yichenjake@utexas.edu>
2021-11-03 22:14:25 -0400

    successfully write data to firebase

--- App.js
+++ App.js
@@ -10,17 +10,42 @@ import CustomMapView from "./components/CustomMapView";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import firebase from "firebase";
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd0HmURysb-s2S8DU9oqH8NafbAemvyzY",
+  authDomain: "freebee-22ac9.firebaseapp.com",
+  projectId: "freebee-22ac9",
+  storageBucket: "freebee-22ac9.appspot.com",
+  messagingSenderId: "729239388132",
+  appId: "1:729239388132:web:5379c961203fdd0afb11f1",
+  measurementId: "G-T5DGFMK68D",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+const db = app.firestore();
+
+db.collection("users")
+  .doc("Yichen")
+  .set({
+    firstName: "Yichen",
+    lastName: "Zhang",
+    school: "Georgia Tech",
+  })
+  .then(() => {
+    console.log("Document successfully written!");
+  })
+  .catch((error) => {
+    console.error("Error writing document: ", error);
+  });
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    // <View style={styles.container}>
-    //   <Text>Hello</Text>
-    // </View>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
